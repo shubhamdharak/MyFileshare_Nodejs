6b65b62a9fad4cc06e7f8f4a294e9a1d304575c2
shubhamdharak <shubhamdharak1133@gmail.com>
2020-09-10 20:48:40 +0530

    added schedular

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express()
-const path = require('path')
+const path = require('path');
+const File = require('./models/file');
+const fs = require('fs')
 const port = process.env.PORT || 3000
 require('./initDB')
 const core = require('cors');
+// const { delete } = require('./routes/show');
+// // const { delete } = require('./routes/show');
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -20,6 +24,24 @@ app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'))
 
+
 app.listen(port, ()=> {
-    console.log(`Server running on ${port}`);
-})
+     setInterval(async()=> {
+        const files =  await File.find({ createdAt : { $lt: new Date(Date.now() - 60 * 1000)} })
+            if(files.length) {
+                for (const file of files) {
+                    try {
+                        fs.unlinkSync(file.filePath);
+                        await file.remove();
+                        console.log(`successfully deleted ${file.fileName}`);
+                    } catch(err) {
+                        console.log(`error while deleting file ${err} `);
+                    }
+                }
+            }
+            console.log('Job done!');
+    }, 2 * 60 * 1000);
+        
+            // deleteFile().then(process.exit);
+    
+});
